7076ac31f7a2f08b105dc6a2ece799041da37ca5
Atherv Gole <agole@ucsb.edu>
2025-04-22 21:54:19 -0700

    glassmorphism layout

--- src/components/ui/SampleList.jsx
+++ src/components/ui/SampleList.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function SampleList() { 
     return (
-        <div className='container mx-auto bg-slate-300/30 rounded-lg h-[calc(100vh-9rem)]'>
+        <div className='container mx-auto inset-shadow-xs inset-shadow-slate-100/50 shadow-lg shadow-gray-700/70 bg-slate-300/30 rounded-lg h-[calc(100vh-9rem)]'>
             
         </div>
     );
