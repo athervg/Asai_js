8dc185c357bd76e9888223c04abb80f9354ea63b
Atherv Gole <agole@ucsb.edu>
2025-07-07 18:19:37 -0700

    StableUI
    first stable UI layout before sampleList

--- src/components/ui/MediaControls.jsx
+++ src/components/ui/MediaControls.jsx
@@ -2,8 +2,7 @@ import React from 'react';
 
 function MediaControls() { 
     return (
-        <div className='container mx-auto inset-shadow-xs inset-shadow-slate-100/50 shadow-lg shadow-gray-700/70 bg-slate-300/30 rounded-lg h-16 mt-4'>
-            
+        <div className='container mx-auto inset-shadow-xs inset-shadow-slate-100/20 shadow-lg shadow-gray-700/70 bg-slate-300/30 rounded-lg h-16 mt-4'>        
         </div>
     );
 }
